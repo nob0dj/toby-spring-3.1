aa50fe28c08eab83cbaca11b6bd7cd0667239968
shqkel <shqkel@gmail.com>
2020-12-30 21:46:46 +0900

    스프링관련 comment 추가!

--- spring31-1.5.1/src/springbook/user/dao/DaoFactory.java
+++ spring31-1.5.1/src/springbook/user/dao/DaoFactory.java
@@ -5,6 +5,12 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class DaoFactory {
+	
+	/**
+	 * 메소드명이 빈의 이름이 된다.
+	 * 
+	 * @return
+	 */
 	@Bean
 	public UserDao userDao() {
 		UserDao dao = new UserDao(connectionMaker());
